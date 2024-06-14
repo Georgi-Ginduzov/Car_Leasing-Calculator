1110441de71dfab1915d1f2d6e68584a17cae006
Georgi <georgi.ginduzov17d@gmail.com>
2024-06-14 19:31:12 +0300

    Calculation improvement

--- scripts/calculations.js
+++ scripts/calculations.js
@@ -1,22 +1,20 @@
 function calculateLeasingDetails() {
-    const carValue = Number(document.getElementById('car_value').value);
-    const leasePeriod = document.getElementById('lease_period').value;
-    const downPaymentPercentage = document.getElementById('down_payment').value;
-    
-    const downPayment = Number(carValue * (1 + (downPaymentPercentage / 100)));
-    
-    
+    // From input data: 
     const interestRate = parseFloat(document.getElementById('interes_rate_output').innerText);
-    const loanAmount = Number(carValue - downPayment);
-    const monthlyInterestRate = Number(interestRate / 100 / 12);
-    const numberOfPayments = Number(leasePeriod);
+    const totalCarValue = parseFloat(document.getElementById('car_range_value').value);
+    const leasePeriodInMonths = document.getElementById('lease_period').value;
+    const downPaymentPercentage = document.getElementById('down_range_payment').value;    
 
-    const monthlyInstallment = loanAmount * monthlyInterestRate / (1 - (Math.pow(1 / (1 + monthlyInterestRate), numberOfPayments)));
-    const totalLeasingCost = Number(monthlyInstallment * numberOfPayments + downPayment);
+    const downPayment = totalCarValue * (downPaymentPercentage / 100);
+    const loanAmount = totalCarValue - downPayment;
+    const monthlyInterestRate = interestRate / 100 / 12;
+    const numberOfPayments = leasePeriodInMonths;
+    const monthlyInstallment = (loanAmount * monthlyInterestRate) / (1 - (Math.pow(1 / (1 + monthlyInterestRate), numberOfPayments)));
+    const totalLeasingCost = parseFloat(monthlyInstallment * numberOfPayments + downPayment);
 
     document.getElementById('total_leasing_cost').innerText = totalLeasingCost.toFixed(2);
     document.getElementById('down_payment_output').innerText = downPayment.toFixed(2);
-    document.getElementById('monthly_installment').innerText = 1940.12;//monthlyInstallment.toFixed(2);
+    document.getElementById('monthly_installment').innerText = monthlyInstallment.toFixed(2);
 }
 
 calculateLeasingDetails();
